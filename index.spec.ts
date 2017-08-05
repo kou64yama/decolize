b48a35b3ff2688b08e53d8125c188633b4616998
YAMADA Koji <kou64yama@gmail.com>
2017-08-05 12:59:17 +0900

    Make sure that the coverage is 100%

--- index.spec.ts
+++ index.spec.ts
@@ -2,7 +2,7 @@ import index from './index';
 import Decolize from './lib/Decolize';
 
 describe('index', () => {
-    it('exports the Decolize class', () => {
+    it('exports the Decolize class.', () => {
         expect(index).toBe(Decolize);
     });
 });
